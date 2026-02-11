24ce4f09d4fb4d5fb5c873612f938d0fa5e284fc
blancayessenia <blancayesseniae@gmail.com>
2026-02-10 17:30:44 -0800

    python basico

--- sesion_01.py
+++ sesion_01.py
@@ -23,3 +23,47 @@ print(float(25%4))
 print(float(16%2))
 print(float(10/3))
 
+# variables
+print("======Variables=====")
+x=100
+y=1
+print(x+y)
+
+ventas=1999991
+print("Nuestras ventas fueron:",ventas)
+
+is_active =True 
+print(is_active)
+
+game_over = False
+print(game_over)
+
+some_string = "Hola soy un string" 
+print(some_string)
+
+print("=====Condicionales=====")
+edad = 15
+
+if (edad>=18):
+    print("Si puedes entrar al bar")
+else:
+    print("No puedes entrar al bar")
+
+mi_numero = int(input("¿Cual es el numero que deseas verificar?:"))
+print(f"El numero que deseas verificar es {mi_numero}")
+if mi_numero % 2 == 0:
+    print(f"El numero{mi_numero} es par")
+else:
+    print(f"El numero{mi_numero} es impar")
+
+def par_impar(numero):
+    if numero % 2 == 0:
+        print(f"El numero {numero} es par")
+    else:
+        print(f"El numero {numero} es impar")
+        
+print("=====FUNCIONPAR IMPAR=====")
+
+mi_numero = int(input("¿Cual es el numero que deseas verificar?:"))
+print(f"El numero que deseas verificar es{mi_numero}")
+print(par_impar(mi_numero))
